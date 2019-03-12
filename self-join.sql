c1ecfea6270bcedc3448ce32dac2ca0b5bb0ed79
Avi Agarwal <avidiotic@users.noreply.github.com>
2019-03-12 19:15:05 +0530

    Update self-join.sql

--- self-join.sql
+++ self-join.sql
@@ -53,17 +53,17 @@ WHERE stopb.name = 'Craiglockhart'
 
 
 /* Q10 */
-SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
-FROM route a 
-JOIN route b ON (a.company = b.company AND a.num = b.num)
-JOIN ( route c 
-        JOIN route d 
-        ON (c.company = d.company AND c.num= d.num))
-JOIN stops stopa ON (a.stop = stopa.id)
-JOIN stops stopb ON (b.stop = stopb.id)
-JOIN stops stopc ON (c.stop = stopc.id)
-JOIN stops stopd ON (d.stop = stopd.id)
+SELECT DISTINCT routea.num, routea.company, stopb.name ,  routec.num,  routec.company
+FROM route routea 
+JOIN route routeb ON (routea.company = routeb.company AND routea.num = routeb.num)
+JOIN ( route routec 
+        JOIN route routed 
+        ON (routec.company = routed.company AND routec.num= routed.num))
+JOIN stops stopa ON (routea.stop = stopa.id)
+JOIN stops stopb ON (routeb.stop = stopb.id)
+JOIN stops stopc ON (routec.stop = stopc.id)
+JOIN stops stopd ON (routed.stop = stopd.id)
 WHERE  stopa.name = 'Craiglockhart'
-        AND stopd.name = 'Sighthill'
+        AND stopd.name = 'Lochend'
         AND stopb.name = stopc.name
-ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
+ORDER BY LENGTH(routea.num), routeb.num, stopb.id, LENGTH(routec.num), routed.num
